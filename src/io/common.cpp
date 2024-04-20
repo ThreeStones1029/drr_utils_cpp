67e4f3ce8107c3a2591aee010a57990817be70e6
ThreeStones1029 <2320218115@qq.com>
2024-04-20 06:41:25 +0000

    update

--- src/io/common.cpp
+++ src/io/common.cpp
@@ -4,7 +4,7 @@
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-04-20 02:57:42
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-04-20 03:13:25
+ * @LastEditTime: 2024-04-20 06:38:14
  */
 #include "common.h"
 
@@ -12,6 +12,7 @@ void remove_all_files(const std::filesystem::path& dir_path) {
     for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
         if (std::filesystem::is_directory(entry)) {
             remove_all_files(entry.path()); // 递归删除子文件夹中的文件和文件夹
+            std::filesystem::remove(entry.path());
         } else {
             std::filesystem::remove(entry); // 删除文件
         }
